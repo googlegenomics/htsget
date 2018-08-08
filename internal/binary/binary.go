fe4b5b418f4fcd02c03842c595d4fd82fcb3e655
Ana Manolache <anamano@google.com>
2018-08-08 13:47:37 -0400

    Extracted binary methods to a new package

---
+++ internal/binary/binary.go
@@ -0,0 +1,40 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package binary provides support for binary operations on files.
+package binary
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+// ChecksMagic checks the magic bytes from the provided reader.
+func CheckMagic(r io.Reader, want []byte) error {
+	got := make([]byte, len(want))
+	if _, err := io.ReadFull(r, got); err != nil {
+		return fmt.Errorf("reading magic: %v", err)
+	}
+	if !bytes.Equal(got, want) {
+		return fmt.Errorf("wrong magic %v (wanted %v)", got, want)
+	}
+	return nil
+}
+
+// Read reads the value from the provided reader into the provided interface.
+func Read(r io.Reader, v interface{}) error {
+	return binary.Read(r, binary.LittleEndian, v)
+}
---
+++ internal/binary/binary_test.go
@@ -0,0 +1,44 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package binary
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCheckMagic(t *testing.T) {
+	testCases := []struct {
+		magic []byte
+		input []byte
+		match bool
+	}{
+		{[]byte("BCF\x02\x02"), []byte("BCF\x02\x02"), true},
+		{[]byte("BCF\x02\x02"), []byte("BCF\x02\x02EXTRA"), true},
+		{[]byte("BCF\x02\x02"), []byte("BCF\x02"), false},
+		{[]byte("BCF\x02\x02"), []byte(""), false},
+	}
+
+	for i, tc := range testCases {
+		t.Run(string(i), func(t *testing.T) {
+			err := CheckMagic(bytes.NewReader(tc.input), tc.magic)
+			if err != nil && tc.match {
+				t.Fatalf("CheckMagic rejected valid magic %v: %v", tc.match, err)
+			} else if err == nil && !tc.match {
+				t.Fatalf("CheckMagic accepted invalid magic %v", tc.match)
+			}
+		})
+	}
+}
