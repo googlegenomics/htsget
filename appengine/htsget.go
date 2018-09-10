62a49ffe47c8e78921a0dfb42c602d27f82e9e59
Ana Manolache <anamano@google.com>
2018-09-10 11:25:41 -0400

    Revert "fixed context problem for gcloud app engine"
    This reverts commit 42c39b0a8efb355c0d5b3a17f6fb2e2c5352ba9f.

--- appengine/htsget.go
+++ appengine/htsget.go
@@ -1,46 +1,25 @@
 package htsget
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"strings"
-	"sync"
 
 	"cloud.google.com/go/storage"
 	"github.com/googlegenomics/htsget/internal/api"
-	"google.golang.org/api/option"
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/urlfetch"
 )
 
 func init() {
-	http.HandleFunc("/", handleRequest)
-}
-
-var (
-	mux              *http.ServeMux
-	initializeServer sync.Once
-)
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	initializeServer.Do(func() {
-		mux = http.NewServeMux()
-		server := api.NewServer(newAppEngineClient, 8*1024*1024)
-		if list := os.Getenv("BUCKET_WHITELIST"); list != "" {
-			server.Whitelist(strings.Split(list, ","))
-		}
-		server.Export(mux)
-	})
-	mux.ServeHTTP(w, r)
+	mux := http.NewServeMux()
+	server := api.NewServer(newAppEngineClient, 8*1024*1024)
+	if list := os.Getenv("BUCKET_WHITELIST"); list != "" {
+		server.Whitelist(strings.Split(list, ","))
+	}
+	server.Export(mux)
+	http.HandleFunc("/", mux.ServeHTTP)
 }
 
 func newAppEngineClient(req *http.Request) (*storage.Client, http.Header, error) {
-	ctx := appengine.NewContext(req)
-	gcs, err := storage.NewClient(ctx, option.WithHTTPClient(urlfetch.Client(ctx)))
-	if err != nil {
-		return nil, nil, fmt.Errorf("creating storage client: %v", err)
-	}
-
-	return gcs, nil, nil
+	return api.NewClientFromBearerToken(req.WithContext(appengine.NewContext(req)))
 }
